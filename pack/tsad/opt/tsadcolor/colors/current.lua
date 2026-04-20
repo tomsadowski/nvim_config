c84b97b76ec04525c7158c7d0d380b525e9aeca4
tomsadowski <tsadowski93@gmail.com>
2026-04-20 09:00:08 -0400

    al

--- pack/tsad/opt/tsadcolor/colors/current.lua
+++ pack/tsad/opt/tsadcolor/colors/current.lua
@@ -9,16 +9,16 @@ local p2 = vim.deepcopy(c.palette)
   p.dblue.g    = "#35383c"
   p.dgrey.g    = "#585c60"
   p.grey.g     = "#757a80"
-  p.white.g    = "#ffffff"
+  p.white.g    = "#f0f0f0"
 
   p.dyellow.g  = "#203018"
   p.dcyan.g    = "#103040"
 
   p.red.g      = "#ff6060"
   p.yellow.g   = "#c0b050"
-  p.cyan.g     = "#88b8ff"
+  p.cyan.g     = "#88c0ff"
   --p.magenta.g  = "#c888ff"
-  p.magenta.g  = "#c878ff"
+  p.magenta.g  = "#d078d0"
 
 c.apply {
   canvas     = p.black, 
