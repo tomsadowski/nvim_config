23591c878859e7000e3b75fd0ed40ec2bd9c8b04
tomsadowski <tsadowski93@gmail.com>
2026-04-24 10:39:39 -0400

    version 2

--- pack/tsad/opt/tsadcolor/colors/fire2.lua
+++ pack/tsad/opt/tsadcolor/colors/fire2.lua
@@ -1,4 +1,4 @@
--- spring
+-- fire
 
 require "treesitter"
 local c = require "color"
@@ -14,10 +14,10 @@ local p2 = vim.deepcopy(c.palette)
   p.dyellow.g  = "#3c3a20"
   p.dcyan.g    = "#203850"
 
-  p.red.g      = "#f07828"
-  p.yellow.g   = "#b8c020"
-  p.cyan.g     = "#88c8ff"
-  p.magenta.g  = "#f07cf0"
+  p.red.g      = "#ff6060"
+  p.yellow.g   = "#d8b840"
+  p.cyan.g     = "#80d0d8"
+  p.magenta.g  = "#e080e0"
 
 c.apply {
   canvas     = p.black, 
