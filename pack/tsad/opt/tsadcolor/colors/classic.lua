1efcfe784305eccf1abfc3615db4619b095cea23
tomsadowski <tsadowski93@gmail.com>
2026-05-15 12:58:49 -0400

    aye

--- pack/tsad/opt/tsadcolor/colors/classic.lua
+++ pack/tsad/opt/tsadcolor/colors/classic.lua
@@ -1,33 +1,29 @@
--- classic
+-- light
 
 require "treesitter"
 local c = require "color"
 local p = vim.deepcopy(c.palette)
 
 c.apply {
-  canvas     = p.white, 
-
   search     = p.cyan, 
   visual     = p.yellow, 
   diagund    = p.green,
-  signcol    = p.dgreen, 
-
-  lineno     = p.grey, 
+  canvas     = p.white, 
+  signcol    = p.dgrey, 
+  lineno     = p.dgrey, 
   msgarea    = p.dgrey, 
-
-  comment    = p.dgreen, 
-  keyword    = p.blue,
-  constant   = p.red, 
-  literal    = p.red, 
-  str        = p.red, 
-  type       = p.black, 
-
-  normal     = p.dgrey, 
-  typeparam  = p.black,
+  comment    = p.dgrey, 
+  normal     = p.black, 
   curlineno  = p.black, 
-  item       = p.black, 
-  matchparen = p.black, 
-  func       = p.black, 
-  variable   = p.black, 
-  btype      = p.black, 
+  keyword    = p.red,
+  variable   = p.blue, 
+  typeparam  = p.blue,
+  type       = p.dgreen, 
+  item       = p.dgreen, 
+  matchparen = p.dgreen, 
+  func       = p.dgreen, 
+  btype      = p.dgreen, 
+  constant   = p.magenta, 
+  literal    = p.magenta, 
+  str        = p.magenta, 
 }
--- pack/tsad/opt/tsadcolor/colors/current.lua
+++ pack/tsad/opt/tsadcolor/colors/current.lua
@@ -4,39 +4,26 @@ require "treesitter"
 local c = require "color"
 local p = vim.deepcopy(c.palette)
 
-p.grey.g    = "#e0e0e0"
-p.dgreen.g  = "#307000"
-p.magenta.g = "#c000f0"
-p.blue.g    = "#0050e0"
-p.red.g     = "#e02000"
-
 c.apply {
-  canvas     = p.white, 
-
   search     = p.cyan, 
   visual     = p.yellow, 
-  diagund    = p.grey,
-
+  diagund    = p.green,
+  canvas     = p.white, 
   signcol    = p.dgrey, 
   lineno     = p.dgrey, 
   msgarea    = p.dgrey, 
   comment    = p.dgrey, 
-
   normal     = p.black, 
   curlineno  = p.black, 
-
+  keyword    = p.red,
   variable   = p.blue, 
   typeparam  = p.blue,
-
   type       = p.dgreen, 
   item       = p.dgreen, 
   matchparen = p.dgreen, 
   func       = p.dgreen, 
   btype      = p.dgreen, 
-
   constant   = p.magenta, 
   literal    = p.magenta, 
   str        = p.magenta, 
-
-  keyword    = p.red,
 }
