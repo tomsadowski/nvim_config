ebc1efedb8684f739bcd200ea59831aa731b0f71
tomsadowski <tsadowski93@gmail.com>
2026-06-16 10:00:54 -0400

    nuked all but ball

--- pack/tsad/opt/tsadcolor/colors/current.lua
+++ pack/tsad/opt/tsadcolor/colors/current.lua
@@ -1,21 +1,21 @@
--- fire
+-- ball
 
 require "treesitter"
 local c = require "color"
 local p = vim.deepcopy(c.palette)
 
-p.black.g    = "#1c1c1c"
-p.dgrey.g    = "#787878"
-p.white.g    = "#d8d8d8"
+p.black.g    = "#202020"
+p.dgrey.g    = "#808080"
+p.white.g    = "#e0e0e0"
 
 p.dmagenta.g = "#402028"
 p.dyellow.g  = "#2c3020"
 p.dcyan.g    = "#20302c"
 
-p.red.g      = "#f06050"
-p.yellow.g   = "#c8b040"
-p.green.g    = "#60b08c"
-p.cyan.g     = "#8098f0"
+p.red.g      = "#f07060"
+p.yellow.g   = "#d8c050"
+p.green.g    = "#70d0bc"
+p.cyan.g     = "#a0c0ff"
 p.magenta.g  = "#d88090"
 
 c.apply {
