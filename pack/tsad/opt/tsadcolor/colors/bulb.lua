f9bc47193809be0a7b461a5fd9e2258f16ddbb19
tomsadowski <tsadowski93@gmail.com>
2026-06-29 14:53:26 -0400

    bulb is good

--- pack/tsad/opt/tsadcolor/colors/bulb.lua
+++ pack/tsad/opt/tsadcolor/colors/bulb.lua
@@ -7,17 +7,17 @@ local p = vim.deepcopy(c.palette)
 p.black.g    = "#202830"
 p.dgrey.g    = "#747a80"
 p.grey.g     = "#c0c8d0"
-p.white.g    = "#f0d8b8"
+p.white.g    = "#e0c8b8"
 
 p.dred.g     = "#3c2c30"
 p.dyellow.g  = "#504c30"
 p.dblue.g    = "#244050"
 
-p.magenta.g  = "#ffb0b8"
+p.magenta.g  = "#f0b0b8"
 p.red.g      = "#ff8070"
 p.yellow.g   = "#e0b870"
-p.cyan.g     = "#b0d6c8"
-p.blue.g     = "#d8bcff"
+p.cyan.g     = "#b0d0c8"
+p.blue.g     = "#d8bcf0"
 
 local CONSTANT = ""
 
