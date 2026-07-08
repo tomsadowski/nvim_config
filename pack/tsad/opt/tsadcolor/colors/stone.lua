488a4b5830567ed4e3e969e99aa3c06ccf6ff845
tom <tsadowski93@gmail.com>
2026-07-07 21:25:04 -0400

    longshoreman

--- pack/tsad/opt/tsadcolor/colors/stone.lua
+++ pack/tsad/opt/tsadcolor/colors/stone.lua
@@ -5,17 +5,17 @@ local c = require "color"
 local p = vim.deepcopy(c.palette)
 
 p.black.g    = "#141a20"
-p.dgrey.g    = "#687078"
+p.dgrey.g    = "#607080"
 p.white.g    = "#c8b6a8"
 
 p.dred.g     = "#402028"
 p.dyellow.g  = "#403c28"
 p.dblue.g    = "#183040"
 
-p.magenta.g  = "#c08ca0"
-p.red.g      = "#c06060"
-p.yellow.g   = "#b89868"
-p.cyan.g     = "#969ad0"
+p.magenta.g  = "#c09298"
+p.red.g      = "#d07060"
+p.yellow.g   = "#c09868"
+p.cyan.g     = "#90a4c8"
 p.dcyan.g    = "#7ca8a0"
 
 local CONSTANT = ""
