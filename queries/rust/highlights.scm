1e960075024f9e6d453357e8be1e62630e3f02ec
tom <tsadowski93@gmail.com>
2026-08-05 09:30:40 -0400

    rust treesitter field_initializer field_identifier is variable

--- queries/rust/highlights.scm
+++ queries/rust/highlights.scm
@@ -341,7 +341,7 @@ function: (scoped_identifier
 (field_identifier) @variable
 (field_pattern     name: (field_identifier) @normal)
 (field_declaration name: (field_identifier) @variable.member.declaration)
-(field_initializer       (field_identifier) @normal)
+(field_initializer       (field_identifier) @variable)
 
 (field_expression field: (field_identifier) @variable.member)
 (field_expression      
