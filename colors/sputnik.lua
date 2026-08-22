5f9b7ab2a8c24b920f3561dfe11795263ede5289
tom <tsadowski93@gmail.com>
2026-08-22 13:31:17 -0400

    sputnik

--- colors/sputnik.lua
+++ colors/sputnik.lua
@@ -1,20 +1,19 @@
--- tricolor
+-- sputnik
 
 require "treesitter"
 local c = require "color"
 local p = vim.deepcopy(c.palette)
 
-p.black.g    = "#202020"
-p.dgrey.g    = "#909090"
-p.white.g    = "#f0e8d8"
+p.black.g    = "#202830"
+p.dgrey.g    = "#8090a0"
+p.white.g    = "#e0d8c8"
 
 p.dmagenta.g = "#402830"
 p.dyellow.g  = "#403c30"
 p.dblue.g    = "#303030"
 
-p.magenta.g  = "#e0a0c0"
-p.yellow.g   = "#b0c860"
-p.cyan.g     = "#a0c0ff"
+p.cyan.g     = "#90b8ff"
+p.magenta.g  = "#c890b8"
 
 
 local CONSTANT = ""
@@ -29,24 +28,26 @@ c.apply {
   signcol    = p.dgrey, 
   lineno     = p.dgrey, 
   msgarea    = p.dgrey, 
-  comment    = p.dgrey, 
+  keyword    = p.dgrey,
+
 
   normal     = p.dgrey, 
+  path       = p.white, 
+  trunk      = p.white,
   curlineno  = p.white, 
   import     = p.white,
-  trunk      = p.white,
-  path       = p.white, 
   variant    = p.white, 
   variable   = p.white, 
   func       = p.white, 
+  typeparam  = p.white,
+  item       = p.white, 
+  type       = p.white, 
+  btype      = p.white, 
+  constant   = p.white, 
 
-  keyword    = p.magenta,
-  typeparam  = p.yellow,
-  item       = p.yellow, 
-  type       = p.yellow, 
-  btype      = p.yellow, 
-  constant   = p.yellow, 
-  literal    = p.cyan, 
-  str        = p.cyan, 
-  matchparen = p.cyan, 
+  comment    = p.cyan, 
+
+  literal    = p.magenta, 
+  str        = p.magenta, 
+  matchparen = p.magenta, 
 }
