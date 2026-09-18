11d56c1941bb08aeacb6c9bef97399b13580ae7e
tom <tsadowski93@gmail.com>
2026-09-18 19:07:46 -0400

    u

--- colors/moss.lua
+++ colors/moss.lua
@@ -6,12 +6,12 @@ local p = vim.deepcopy(c.palette)
 
 p.black.g    = "#202225"
 p.dgrey.g    = "#707880"
-p.grey.g     = "#b8b9c0"
+p.grey.g     = "#aaabb0"
 p.white.g    = "#b8a098"
 
 p.green.g    = "#bca47c"
-p.cyan.g     = "#78a090"
-p.magenta.g  = "#c698ac"
+p.cyan.g     = "#709080"
+p.magenta.g  = "#c694ac"
 p.red.g      = "#e08c78"
 
 p.dmagenta.g = "#402c40"
